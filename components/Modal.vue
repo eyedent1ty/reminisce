fc35486eadc1e2796c869eb691cf4853012a1dc8
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-03-13 10:02:01 +0800

    created a resuable button

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="absolute bg-black opacity-60 top-0 left-0 right-0 bottom-0"></div>
   <div
-    class="bg-white rounded w-1/2 max-w-2xl max-h-40 z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    class="bg-white rounded w-11/12 sm:w-1/2 lg:w-1/3 max-w-2xl max-h-40 z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-3"
   >
-    <header class="flex justify-between items-center min-h-12 px-3 border-b">
+    <header class="flex justify-between items-center min-h-12 border-b font-bold">
       <h3 class="text-2xl">Confirmation</h3>
       <button type="button">X</button>
     </header>
@@ -12,9 +12,9 @@
     >
       Are you sure you want to restart the game?
     </main>
-    <footer class="flex justify-end items-center min-h-12">
-      <button>Cancel</button>
-      <button>Confirm</button>
+    <footer class="flex justify-end items-center min-h-12 gap-2">
+      <TheButton>Cancel</TheButton>
+      <TheButton primary>Confirm</TheButton>
     </footer>
   </div>
 </template>
---
+++ components/TheButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-if="primary" :class="[{ 'bg-primary': primary }, computedClasses]">
+    <slot />
+  </button>
+  <button v-else :class="[{ 'bg-secondary': !primary }, computedClasses]">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  primary: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const computedClasses = computed(() => 'min-w-24 min-h-10 rounded font-bold');
+</script>
